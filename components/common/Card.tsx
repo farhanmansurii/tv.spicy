225b7a14810dcb372f4a908f5ce1ecbd0659c2eb
Farhan Mansuri <mansurifarhanfm@gmail.com>
2023-09-29 20:12:55 +0530

    dd

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -6,7 +6,7 @@ function Card(props:any) {
 
   return (
     <Link key={data.id} href={`/${type}/${data.id}`}>
-      <div className="flex-none  hover:z-10 -z-10  md:hover:scale-105 md:hover:shadow-2xl overscroll-y-none duration-200  h-72 w-32 md:w-48">
+      <div className="flex-none  hover:z-10 -z-10  md:hover:scale-105 md:hover:shadow-2xl overscroll-y-none duration-200   h-58 md:h-72 w-32 md:w-48">
           <img
             className="object-cover  "
             src={`https://image.tmdb.org/t/p/w500${data.poster_path}` || data.image}
