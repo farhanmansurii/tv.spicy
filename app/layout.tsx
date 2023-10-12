2d2d6b7d3da88d706e7e5cb46663b6e78ba9fe69
Farhan Mansuri <mansurifarhanfm@gmail.com>
2023-10-13 02:33:48 +0530

    dsadsa

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+          <meta name="theme-color" content="#e63946" />
+          <title>Spicy TV</title>
+        </head>
       <body>
         <Providers
           themes={["redDark", "redLight", "light", "dark"]}
