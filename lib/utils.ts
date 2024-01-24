b6618d57b966ae4974fcfe58cbedbb2a51e3d5a9
farhan <74182335+farhanmansurii@users.noreply.github.com>
2024-01-25 02:59:17 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,7 +22,7 @@ export async function fetchRowData(type: string) {
 export async function fetchDetails(id: string, type: string) {
   try {
     const url = new URL(
-      `https://spicy-anime-api.vercel.app/meta/tmdb/info/${id}?type=${type}`
+      `'https://api-spicy.vercel.app/meta/tmdb/info/${id}?type=${type}`
     );
     const response = await fetch(url.toString(), { cache: 'no-cache' });
     if (!response.ok) throw new Error('Failed to fetch data');
@@ -69,7 +69,7 @@ export async function fetchMovieLinks(
 ) {
   try {
     const url = new URL(
-      `https://spicy-anime-api.vercel.app/movies/flixhq/watch?episodeId=${movie}&mediaId=${longID}&server=vidcloud`
+      `https://api-spicy.vercel.app/movies/flixhq/watch?episodeId=${movie}&mediaId=${longID}&server=vidcloud`
     );
     const response = await fetch(url.toString());
     if (!response.ok) throw new Error('Failed to fetch data');
