0f1af0eab13bdbfe412d8e07d1d29cdce07f83a8
Farhan Mansuri <mansurifarhanfm@gmail.com>
2024-06-19 05:04:14 +0530

    sss

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,9 +1,20 @@
 import { GeistSans } from "geist/font/sans";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
+      {children}
+    </div>
+  );
 }
