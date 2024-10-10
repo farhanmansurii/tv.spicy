934daeb68bec876be2dc48cecfa043e402500f91
Farhan <mansurifarhanfm@gmail.com>
2024-10-11 03:33:28 +0530

    ss

--- components/common/ThemeButton.tsx
+++ components/common/ThemeButton.tsx
@@ -1,38 +1,32 @@
-"use client";
-import React from "react";
+'use client';
+import React from 'react';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
-import { Button } from "../ui/button";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { Button } from '../ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
 export default function ThemeButton() {
-  const { setTheme } = useTheme();
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant={"ghost"}>
-          <Sun className="p-1" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("redLight")}>
-          Red (taylor&apos;s version)
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("redDark")}>
-          Red (dark)
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+	const { setTheme } = useTheme();
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button size={'sm'} variant={'ghost'}>
+					<Sun className="p-1" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end">
+				<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('redLight')}>
+					Red (taylor&apos;s version)
+				</DropdownMenuItem>
+				<DropdownMenuItem onClick={() => setTheme('redDark')}>Red (dark)</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 }
