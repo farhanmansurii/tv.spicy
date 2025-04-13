f8d07ee5d13323fefa7820a7e075ebc956a16133
Farhan Mansuri <mansurifarhanfm@gmail.com>
2025-04-13 06:43:11 +0530

    fix: enhance smooth scrolling and adjust background gradient positioning

--- components/common/BackgroundGradient.tsx
+++ components/common/BackgroundGradient.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const BackgroundGradient = () => {
 	return (
 		<div
-			className="absolute top-0 z-[-2] h-screen w-screen bg-background
+			className="absolute -top-10 z-[-2] h-screen w-screen bg-background
       bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,hsl(var(--primary)/0.5),transparent)]
       dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,hsl(var(--primary)/0.35),transparent)]
       redLight:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,hsl(var(--primary)/0.5),transparent)]
